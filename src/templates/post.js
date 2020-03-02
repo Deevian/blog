f8e1fb202ec9bd593a9caed989a15b5beba0379b
João Abreu <jpmcabreu@gmail.com>
2020-03-02 22:18:33 +0000

    Finish basic blog setup

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,66 +1,9 @@
-import React from 'react'
+import React  from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 
 import { Layout } from '../components/common'
-import { MetaData } from '../components/common/meta'
-
-/**
-* Single post view (/:slug)
-*
-* This file renders a single post and loads all the content.
-*
-*/
-const Post = ({ data, location }) => {
-    const post = data.ghostPost
-
-    return (
-        <>
-            <MetaData
-                data={data}
-                location={location}
-                type="article"
-            />
-            <Helmet>
-                <style type="text/css">{`${post.codeinjection_styles}`}</style>
-            </Helmet>
-            <Layout>
-                <div className="container">
-                    <article className="content">
-                        { post.feature_image ?
-                            <figure className="post-feature-image">
-                                <img src={ post.feature_image } alt={ post.title } />
-                            </figure> : null }
-                        <section className="post-full-content">
-                            <h1 className="content-title">{post.title}</h1>
-
-                            {/* The main post content */ }
-                            <section
-                                className="content-body load-external-scripts"
-                                dangerouslySetInnerHTML={{ __html: post.html }}
-                            />
-                        </section>
-                    </article>
-                </div>
-            </Layout>
-        </>
-    )
-}
-
-Post.propTypes = {
-    data: PropTypes.shape({
-        ghostPost: PropTypes.shape({
-            codeinjection_styles: PropTypes.object,
-            title: PropTypes.string.isRequired,
-            html: PropTypes.string.isRequired,
-            feature_image: PropTypes.string,
-        }).isRequired,
-    }).isRequired,
-    location: PropTypes.object.isRequired,
-}
-
-export default Post
 
 export const postQuery = graphql`
     query($slug: String!) {
@@ -68,4 +11,45 @@ export const postQuery = graphql`
             ...GhostPostFields
         }
     }
-`
+`;
+
+
+const propTypes = {
+    data: PropTypes.shape({
+        ghostPost: PropTypes.shape({
+            codeinjection_styles: PropTypes.object,
+            title: PropTypes.string.isRequired,
+            html: PropTypes.string.isRequired,
+            feature_image: PropTypes.string,
+        }).isRequired,
+    }).isRequired
+};
+
+const Post = ({ data }) => {
+    const post = data.ghostPost;
+
+    return (
+        <Layout>
+            <Helmet>
+                <style type="text/css">{`${post.codeinjection_styles}`}</style>
+                <title>{post.title}</title>
+            </Helmet>
+
+            <main className="post">
+                <nav className="nav active">
+                    <a href="/">
+                        <img alt="Índice" src="/favicon.ico"/>
+                    </a>
+                </nav>
+
+                <h1 className="title">{post.title}</h1>
+                <section dangerouslySetInnerHTML={{ __html: post.html }}/>
+            </main>
+        </Layout>
+
+    )
+};
+
+Post.propTypes = propTypes;
+
+export default Post
