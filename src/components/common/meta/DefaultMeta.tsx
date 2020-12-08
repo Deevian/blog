6abbce63534b5ff9afa2a99fc0706ed1579370d6
João Abreu <jpmcabreu@gmail.com>
2020-12-08 19:51:42 +0000

    Add image cache workaround

--- src/components/common/meta/DefaultMeta.tsx
+++ src/components/common/meta/DefaultMeta.tsx
@@ -14,7 +14,7 @@ export default ({ customStyles }: Props) => (
         <meta property="og:site_name" content="O Ponto Laranja" />
         <meta property="og:title" content="O Ponto Laranja" />
         <meta property="fb:app_id" content="699908233987484" />
-        <meta property="og:image" content="https://opontolaranja.pt/orange.png" />
+        <meta property="og:image" content={`https://opontolaranja.pt/orange.png?v=${new Date().getTime()}`} />
         <meta property="og:description" content="As deambulações cansadas de uma sombra demente" />
         <meta name="twitter:card" content="summary_large_image" />
 
