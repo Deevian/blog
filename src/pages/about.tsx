a804563d5b576b7a1963a413ebd3da11e6e40e56
João Abreu <jabreu@dunelm.com>
2021-03-18 16:47:16 +0000

    Fix about me page formatting

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,6 +2,33 @@ import * as React from 'react';
 import Helmet from 'react-helmet'
 import { Layout, Header } from '../components/common';
 import PostBody from "../components/common/PostBody";
+import { css } from '@linaria/core';
+
+const sectionStyles = css`
+    display: flex;
+    flex-direction: column;
+
+    & > * {
+        align-self: stretch;
+    }
+
+    align-items: center;
+
+    font-weight: normal;
+    line-height: 26px;
+    color: #333;
+`
+
+const titleStyles = css`
+    text-align: center;
+
+    font-size: 1.9rem;
+    font-weight: normal;
+    color: #111;
+
+    margin: 4.5rem 0 1rem;
+`
+
 
 export default () => (
     <Layout>
@@ -11,11 +38,11 @@ export default () => (
 
         <Header />
 
-        <section>
+        <section className={sectionStyles}>
+            <h3 className={titleStyles}>Sobre Mim</h3>
             <PostBody
-                title={'Sobre Mim'}
                 html={`
-                    <p style="margin-top: -1rem">
+                    <p>
                         Olá. O meu nome é João. E eu sou... eu. Uma talvez-pessoa que sabe muito pouco
                         sobre nada, e que volta e meia se perde nas coisas estranhas que populam este ponto
                         laranja. Autor prolífero de blogs defuntos, anda agora à procura das deambulações
