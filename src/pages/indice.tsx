240a8740ac56d2f3f8581d9ec5bd22b8fc60d322
João Abreu <jpmcabreu@gmail.com>
2022-05-22 23:06:41 +0100

    Adding back some stuff

--- src/pages/indice.tsx
+++ src/pages/indice.tsx
@@ -82,10 +82,6 @@ export default ({ data }: Props) => {
                 {data.allGhostPost.edges.map(({ node }) => {
                     const date = dayjs(node.published_at);
                     const year = date.year();
-                    const tags = node.tags;
-
-                    const isPersonal = tags && tags.some(tag => tag.name === 'personal')
-                    if (isPersonal) return null;
 
                     const post = (
                         <Fragment key={node.id}>
