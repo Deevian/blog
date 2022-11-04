3cf9c794d390ebc1ea34a77e3d78c3d1567f0eee
Joao Abreu <joao.abreu@hotjar.com>
2022-11-04 12:28:03 +0000

    Fix layout issue

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -87,14 +87,6 @@ const Layout = ({ showLogo = true, data, children }: Props) => {
             <section className={sectionStyles}>
                 {children}
             </section>
-
-            <script type="text/javascript">
-                (function(c,l,a,r,i,t,y){
-                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "bfed0vq0uj");
-            </script>
         </main>
     )
 }
