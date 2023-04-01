508f843838c4b0b9f69121c3b54bf64e8da409d5
João Abreu <me@deev.io>
2023-04-01 16:47:57 +0100

    add script

--- src/html.js
+++ src/html.js
@@ -1,27 +1,32 @@
 import React from "react";
 
-const fullstoryScriot = `
-  window['_fs_host'] = 'fullstory.com';
-  window['_fs_script'] = 'edge.fullstory.com/s/fs.js';
-  window['_fs_org'] = 'o-1GYGJT-na1';
-  window['_fs_namespace'] = 'FS';
-  (function(m,n,e,t,l,o,g,y){
-  if (e in m) {if(m.console && m.console.log) { m.console.log('FullStory namespace conflict. Please set window["_fs_namespace"].');} return;}
-  g=m[e]=function(a,b,s){g.q?g.q.push([a,b,s]):g._api(a,b,s);};g.q=[];
-  o=n.createElement(t);o.async=1;o.crossOrigin='anonymous';o.src='https://'+_fs_script;
-  y=n.getElementsByTagName(t)[0];y.parentNode.insertBefore(o,y);
-  g.identify=function(i,v,s){g(l,{uid:i},s);if(v)g(l,v,s)};g.setUserVars=function(v,s){g(l,v,s)};g.event=function(i,v,s){g('event',{n:i,p:v},s)};
-  g.anonymize=function(){g.identify(!!0)};
-  g.shutdown=function(){g("rec",!1)};g.restart=function(){g("rec",!0)};
-  g.log = function(a,b){g("log",[a,b])};
-  g.consent=function(a){g("consent",!arguments.length||a)};
-  g.identifyAccount=function(i,v){o='account';v=v||{};v.acctId=i;g(o,v)};
-  g.clearUserCookie=function(){};
-  g.setVars=function(n, p){g('setVars',[n,p]);};
-  g._w={};y='XMLHttpRequest';g._w[y]=m[y];y='fetch';g._w[y]=m[y];
-  if(m[y])m[y]=function(){return g._w[y].apply(this,arguments)};
-  g._v="1.3.0";
-  })(window,document,window['_fs_namespace'],'script','user');
+const ss = `
+  var _paq = window._paq || [];
+  _paq.push(["setRequestMethod", "POST"]);
+  _paq.push(["setCookieDomain", "opontolaranja.pt"]);
+  _paq.push(["setDomains",["opontolaranja.pt"],
+  ]);
+  _paq.push(["trackPageView"]);
+  _paq.push(["enableLinkTracking"]);
+  _paq.push(["enableHeartBeatTimer"]);
+  if (document.referrer) {
+    _paq.push(["setReferrerUrl", document.referrer]);
+  }
+  (function () {
+    var u = "//e.opontolaranja.pt";
+    _paq.push(["setRequestMethod", "POST"]);
+    _paq.push(["setTrackerUrl", u]);
+    _paq.push(["setSiteId", "1"]);
+
+    var d = document,
+      g = d.createElement("script"),
+      s = d.getElementsByTagName("script")[0];
+    g.type = "text/javascript";
+    g.async = true;
+    g.defer = true;
+    g.src = "https://opontolaranja.pt/m.js";
+    s.parentNode.insertBefore(g, s);
+  })();
 `;
 
 export default function HTML(props) {
@@ -43,7 +48,7 @@ export default function HTML(props) {
         />
         {props.postBodyComponents}
 
-        <script async dangerouslySetInnerHTML={{ __html: fullstoryScriot }} />
+        <script async dangerouslySetInnerHTML={{ __html: ss }} />
       </body>
     </html>
   );
